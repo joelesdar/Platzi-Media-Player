226c0eccffa21f8d7ad170abb76f24aabbdcc901
Joel Esteban Diaz Arévalo <joel-e11@hotmail.com>
2023-02-03 23:13:44 -0500

    Intersection observer, service workers y visibility change agregados

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,18 @@
 import MediaPlayer from './MediaPlayer.js'
 import AutoPlay from './plugins/AutoPlay.js';
+import AutoPause from './plugins/AutoPause.js';
 
 // Seleccionamos las etiquetas (html) que vamos a manipular
 const video = document.getElementById("video");
 const playButton = document.getElementById("play-button");
 const muteButton = document.getElementById("mute-button");
 
-const player = new MediaPlayer({ element: video, plugins: [new AutoPlay] });
+const player = new MediaPlayer({ element: video, plugins: [new AutoPlay, new AutoPause] });
 playButton.onclick = () => player.togglePlay();
 muteButton.onclick = () => player.toggleMute();
+
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch(error => {
+    console.log(error.message);
+  })
+}
